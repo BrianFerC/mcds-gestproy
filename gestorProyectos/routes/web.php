435dacc91309c1bbc7887730de3d9449c92622a0
ALICEOCHOA <fabiolaybrian@gmail.com>
2021-06-13 11:54:55 -0500

    Install Laravel Mixin

--- gestorProyectos/routes/web.php
+++ gestorProyectos/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\LocaleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +21,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('allusers', function () {
+    $user = App\Models\User::take(5)->get();
+    dd($user);
+});
+
+Route::get('showuser/{id}', function (Request $request) {
+    $id = $request->id;
+    $user = App\Models\User::find($id);
+    return view('showuser')->with('user', $user);
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
